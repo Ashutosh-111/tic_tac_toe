d65f8921bd210e7c23a32fd7d33b01649a47a6d6
Ashutosh Gupta <ashutoshgupta3742@gmail.com>
2023-04-02 22:03:14 +0530

    creating board for game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function Board() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Board">
+      <div className="board_row">
+        <button>X</button>
+        <button>X</button>
+        <button>X</button>
+      </div>
+      <div className="board_row">
+        <button>X</button>
+        <button>X</button>
+        <button>X</button>
+      </div>
+      <div className="board_row">
+        <button>X</button>
+        <button>X</button>
+        <button>X</button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
